605229cb6793ba472881f0e8d01c2193ac3e9443
tianlei <18511966208@163.com>
2023-04-13 10:10:03 +0800

    feat：修改数据

--- src/store/registrationList/index.ts
+++ src/store/registrationList/index.ts
@@ -4,64 +4,27 @@ const store={
   state:{ 
     list: [
       {
-        name: '胡彦斌',
+        name: '周深',
         age: 32,
-        address: '西湖区湖底公园1号',
+        address: '北京市朝阳区',
         phone: '18757562024',
         regist_time: 1681309658336
       },
       {
-        name: '胡彦祖',
+        name: '胡歌',
         age: 42,
-        address: '西湖区湖底公园1号',
+        address: '杭州市西湖区',
         phone: '18750564424',
         regist_time: 1681309688169
       },
       {
        
-        name: '胡彦祖2',
+        name: '言承旭',
         age: 22,
-        address: '西湖区湖底公园1号',
+        address: '上海市',
         phone: '18757564545',
         regist_time: 1681309712184
-      },
-      {
-        name: '胡彦祖3',
-        age: 45,
-        address: '西湖区湖底公园1号',
-        phone: '18757568424',
-        regist_time: 1681309714184
-      },
-      {
-       
-        name: '胡彦祖2',
-        age: 22,
-        address: '西湖区湖底公园1号',
-        phone: '18757564524',
-        regist_time: 1681309715184
-      },
-      {
-        name: '胡彦祖3',
-        age: 45,
-        address: '西湖区湖底公园1号',
-        phone: '18757268424',
-        regist_time: 1681309716184
-      },
-      {
-       
-        name: '胡彦祖2',
-        age: 22,
-        address: '西湖区湖底公园1号',
-        phone: '18767564523',
-        regist_time: 1681309816184
-      },
-      {
-        name: '胡彦祖3',
-        age: 45,
-        address: '西湖区湖底公园1号',
-        phone: '19757568420',
-        regist_time: 1681309719184
-      },
+      }
     ]
   },
 
